f7136cbbdfb30cf0cbc59fac1036b19206a2cf39
Suryadeep Singh <180669860+sudosuryadeep@users.noreply.github.com>
2025-05-29 23:33:32 +0000

    variables

---
+++ basics/01_variable.js
@@ -0,0 +1,8 @@
+const account_Id = 12345;
+let account_Email = "learnonix"
+var account_Password = "1114"
+account_City = "Lucknow"
+console.log(account_Id)
+console.log(account_City)
+console.log(account_Email)
+console.log(account_Password)
