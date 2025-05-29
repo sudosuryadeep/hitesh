db5954bb234bf3273d092e316a33bc45f536960f
Suryadeep Singh <180669860+sudosuryadeep@users.noreply.github.com>
2025-05-29 23:34:13 +0000

    data types

---
+++ basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+// "use strict"; //treat all JS code new version ecma javascript 
+// alert(3+3) // we are 
+
+let name = "learnonix"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+    // symbol => unique
+
+console.log(typeof null)
+console.log(typeof undefined);
+console.log(typeof 32)
+console.log(typeof "this is check type")
+console.log(typeof true)
+console.log(typeof false)
+console.log(typeof BigInt)
+console.log(typeof Symbol)
+console.log(typeof bool)
