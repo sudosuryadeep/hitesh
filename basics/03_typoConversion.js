ba34e5bcfaca16826fb7379d9ddb456d7c1af69a
Suryadeep Singh <180669860+sudosuryadeep@users.noreply.github.com>
2025-05-29 23:55:41 +0000

    comparison new

--- basics/03_typoConversion.js
+++ basics/03_typoConversion.js
@@ -1,16 +1,34 @@
 // let score = "33"
 // let score = "33tgja"
-let score = null
-console.log(typeof score)
-console.log(typeof(score))
-console.log(score)
+// let score = null
+// console.log(typeof score)
+// console.log(typeof(score))
+// console.log(score)
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
 
-// "33" => 33
-// "33jkdsh"
-// true => 1; false => 0;
+// // "33" => 33
+// // "33jkdsh"
+// // true => 1; false => 0;
 
+
+// console.log(2+3)
+// console.log(3**4)
+// console.log(3/5)
+// console.log(44%6)
+
+// let str1 = "hello"
+// let str2 = " learnonix"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+console.log(true)
+console.log(+true)
+console.log(true + 2)
+console.log(+"")
+console.log(true - 22)
---
+++ basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log(null > 0)
+console.log(null == 0)
+console.log(null>=0)
+console.log(null<=0)
+console.log(null<0)
+
+
+// strictly check ===
+
+console.log
